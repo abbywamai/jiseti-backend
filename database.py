e1983291fc65075195a01bbf8a6980004f42401d
Abigail Wamai <abigail.wamai@student.moringaschool.com>
2023-10-26 16:29:28 +0300

    switched to sqlite

--- database.py
+++ database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://username:password@localhost/jiseti_db"  # Replace with your database credentials
+DATABASE_URL = "sqlite:///jiseti.db"
 
 engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-from database import Base
+Base = declarative_base()
+
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -15,35 +18,35 @@ class User(Base):
     red_flags = relationship('Red_flag', back_populates='user')
     interventions = relationship('Intervention', back_populates='user')
 
-class Red_flag(Base):
-    __tablename__ = 'redflag'
-    id = Column(Integer, primary_key=True, index=True)
-    incident_type = Column(String)
-    description = Column(String, nullable=False)
-    additional_details = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    userid = Column(Integer, ForeignKey('user.id'))
-    statusid = Column(Integer, ForeignKey('status.id'))
+# class Red_flag(Base):
+#     __tablename__ = 'redflag'
+#     id = Column(Integer, primary_key=True, index=True)
+#     incident_type = Column(String)
+#     description = Column(String, nullable=False)
+#     additional_details = Column(String)
+#     latitude = Column(Float)
+#     longitude = Column(Float)
+#     userid = Column(Integer, ForeignKey('user.id'))
+#     statusid = Column(Integer, ForeignKey('status.id'))
 
-    user = relationship('User', back_populates='red_flags')
+#     user = relationship('User', back_populates='red_flags')
 
-class Intervention(Base):
-    __tablename__ = 'interventions'
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
-    latitude = Column(Float)
-    longitude = Column(Float)
-    userid = Column(Integer, ForeignKey('user.id'))
-    statusid = Column(Integer, ForeignKey('status.id'))
+# class Intervention(Base):
+#     __tablename__ = 'interventions'
+#     id = Column(Integer, primary_key=True, index=True)
+#     title = Column(String)
+#     description = Column(String)
+#     latitude = Column(Float)
+#     longitude = Column(Float)
+#     userid = Column(Integer, ForeignKey('user.id'))
+#     statusid = Column(Integer, ForeignKey('status.id'))
 
-    user = relationship('User', back_populates='interventions')
+#     user = relationship('User', back_populates='interventions')
 
-class Status(Base):
-    __tablename__ = 'status'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+# class Status(Base):
+#     __tablename__ = 'status'
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
 
-    red_flags = relationship('Red_flag', back_populates='status')
-    interventions = relationship('Intervention', back_populates='status')
+#     red_flags = relationship('Red_flag', back_populates='status')
+#     interventions = relationship('Intervention', back_populates='status')
